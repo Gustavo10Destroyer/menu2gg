b7b7dbfc584df35131ea4c09da58f5acb65b089f
Gustavo Vitor Alves <66029954+Gustavo10Destroyer@users.noreply.github.com>
2022-12-15 15:40:05 -0300

    Update exemplo.lua

--- exemplo.lua
+++ exemplo.lua
@@ -1,27 +1,22 @@
 local M2GG = require("menu2gg")
 
-local menu1 = M2GG.Menu()
-local menu2 = M2GG.Menu()
+local menu1 = M2GG.createMenu()
+local menu2 = M2GG.createMenu()
 
-local choice1 = M2GG.Option("Ir para Menu 2", function()
+menu1:createOption("Ir para Menu 2", function()
   menu2:Show()
 end)
 
-local choice2 = M2GG.Option("Sair", function()
+menu1:createOption("Sair", function()
   os.exit()
 end)
 
-local choice3 = M2GG.Option("Voltar para Menu 1", function()
+menu2:createOption("Voltar para Menu 1", function()
   menu1:Show()
 end)
 
-local choice4 = M2GG.Option("Sair", function()
+menu2:createOption("Sair", function()
   os.exit()
 end)
-
-menu1:AddOption(choice1)
-menu1:AddOption(choice2)
-menu2:AddOption(choice3)
-menu2:AddOption(choice4)
 
 menu1:Show()
