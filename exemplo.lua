7b209b72fb676915f5a8e7e8c0301bec2809bfe2
Gustavo Vitor Alves <66029954+Gustavo10Destroyer@users.noreply.github.com>
2022-12-15 16:03:51 -0300

    Update exemplo.lua

--- exemplo.lua
+++ exemplo.lua
@@ -1,22 +1,26 @@
 local M2GG = require("menu2gg")
 
-local menu1 = M2GG.createMenu()
-local menu2 = M2GG.createMenu()
+local menu = M2GG.createMenu()
 
-menu1:createOption("Ir para Menu 2", function()
-  menu2:show()
+menu:createOption("Opção #1", function()
+    gg.toast("Você selecionou a opção #1!")
 end)
 
-menu1:createOption("Sair", function()
+menu:createOption("Desabilitar a 'Opção #1'", function()
+    local targetOption = options:getMenu().options[1]
+    local isEnabled = targetOption:isEnabled()
+
+    if isEnabled then
+      targetOption:disable()
+      self:edit("Habilitar a 'Opção #1'")
+    else
+      targetOption:enable()
+      self:edit("Desabilitar a 'Opção #1'")
+    end
+end)
+
+menu:createOption("Sair", function()
   os.exit()
 end)
 
-menu2:createOption("Voltar para Menu 1", function()
-  menu1:show()
-end)
-
-menu2:createOption("Sair", function()
-  os.exit()
-end)
-
-menu1:show()
+menu:show()
